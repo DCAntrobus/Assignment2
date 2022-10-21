521bf77a269a5c6c2ddb9410a3e8e71d814eacd5
Desmond Antrobus <dantrobu@my.centennialcollege.ca>
2022-10-21 01:38:54 -0400

    Completed CRUD business contact page

--- app/controller/business.contact.list.controller.server.js
+++ app/controller/business.contact.list.controller.server.js
@@ -1,3 +1,4 @@
+import businessContacts from '../models/businessContacts.js';
 import busconlistModel from '../models/businessContacts.js';
 
 export function DisplayContactBusinessList(req, res, next) {
@@ -6,10 +7,71 @@ export function DisplayContactBusinessList(req, res, next) {
             console.error(err);
             res.end(err);
         }
-        res.render('index', { title: 'Business Contact List', page: 'businesscontacts/contactlist', businessContact: businessCollection })
+        res.render('index', { title: 'Business Contact List', page: 'businesscontacts/contactlist', businessContacts: businessCollection })
     })
 }
 
 export function DisplayContactAddPage(req, res, next) {
-    res.render('index', { title: 'Add Contact information', page: 'businesscontacts/listedit', businessContact: {} });
+    res.render('index', { title: 'Add Contact information', page: 'businesscontacts/listedit', businessContacts: {} });
+}
+
+export function ProcessBusinessContactAddPage(req, res, next) {
+    let newContact = busconlistModel({
+        name: req.body.name,
+        phone: req.body.phone,
+        email: req.body.email
+    });
+
+    busconlistModel.create({ newContact }, (err, businessContact) => {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        };
+        res.redirect('/business-contact-list');
+    })
+}
+
+
+export function DisplayContactEditPage(req, res, next) {
+    let id = req.params.id;
+
+    busconlistModel.findById(id, (err, businessContacts) => {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        res.render('index', { title: 'Add Contact information', page: 'businesscontacts/listedit', businessContacts: businessContacts });
+
+    })
+
+}
+export function ProcessBusinessContactEditPage(req, res, next) {
+    let id = req.params.id;
+
+    let newContact = busconlistModel({
+        _id: req.body.id,
+        name: req.body.name,
+        phone: req.body.phone,
+        email: req.body.email
+    });
+
+    busconlistModel.updateOne({ _id: id }, newContact, (err, businessContact) => {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        };
+        res.redirect('/business-contact-list');
+    })
+}
+
+export function ProcessBusinessContactDelete(req, res, next) {
+    let id = req.params.id;
+
+    busconlistModel.remove({ _id: id }, (err) => {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        res.redirect('/business-contact-list');
+    })
 }
--- app/routes/business_Contact_list.route.js
+++ app/routes/business_Contact_list.route.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 
-import { DisplayContactBusinessList, DisplayContactAddPage } from "../controller/business.contact.list.controller.server.js";
+import { DisplayContactBusinessList, DisplayContactAddPage, DisplayContactEditPage, ProcessBusinessContactAddPage, ProcessBusinessContactEditPage, ProcessBusinessContactDelete } from "../controller/business.contact.list.controller.server.js";
 
 const router = Router();
 
 router.get('/business-contact-list', DisplayContactBusinessList);
-router.get('/business-add', DisplayContactAddPage);
+router.get('/business-contact-add', DisplayContactAddPage);
+router.post('/business-contact-add', ProcessBusinessContactAddPage);
+router.post('/business-contact-edit/:id', ProcessBusinessContactEditPage);
+router.get('/business-contact-edit/:id', DisplayContactEditPage);
+router.get('/business-contact-delete/:id', ProcessBusinessContactDelete)
 
 export default router;
