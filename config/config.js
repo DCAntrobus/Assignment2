eb58bce57d792b64a7c34886fe69777b1df28cdc
Desmond Antrobus <dantrobu@my.centennialcollege.ca>
2022-10-21 09:32:20 -0400

    added Mongo Atlas

--- config/config.js
+++ config/config.js
@@ -1,4 +1,2 @@
 export const Secret = "MySecret";
-export const MongoDB = "mongodb://127.0.0.1/media";
-
-
+export const MongoDB = "mongodb+srv://Desmond:Golda47@assignment2.uig1ett.mongodb.net/?retryWrites=true&w=majority";
